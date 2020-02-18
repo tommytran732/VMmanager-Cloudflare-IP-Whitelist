c21c6180a7b0a80930a418d5836f01e6736b1c7f
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2020-02-18 04:56:16 -0500

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -8,9 +8,9 @@ output "Cloudflare IPWhitelist Script for VMmanager 6"
 output "Copyright © 2020 Thien Tran <contact@thientran.io>."
 output "Support: https://thientran.io/discord"
 
-yum -y install curl
-iptables -I DOCKER-USER -p tcp --dport 443 -j DROP
-iptables -I DOCKER-USER -p tcp -s $(curl -sSL https://www.cloudflare.com/ips-v4 | paste -d, -s) --dport 443 -j ACCEPT
+/usr/bin/yum -y install curl
+/sbin/iptables -I DOCKER-USER -p tcp --dport 443 -j DROP
+/sbin/iptables -I DOCKER-USER -p tcp -s $(curl -sSL https://www.cloudflare.com/ips-v4 | paste -d, -s) --dport 443 -j ACCEPT
 
 output "Cloudflare IPv4s have been whitelisted on port 443!"
 output "To make this persistent, run this script at startup. It will also update the list of Cloudflare's IPs."
